be0ce93f7410a558458ef60ea350f0762c731f84
Hari322894 <hmaddineni@ucdavis.edu>
2025-02-24 17:19:45 -0800

    1

--- proj3/testsrc/OpenStreetMapTest.cpp
+++ proj3/testsrc/OpenStreetMapTest.cpp
@@ -7,17 +7,9 @@
 
 class MockXMLReader : public CXMLReader {
 public:
-    struct SXMLEntity {
-        enum class EType { StartElement, EndElement };
-        EType DType;
-        std::string DNameData;
-        std::unordered_map<std::string, std::string> DAttributes;
-    };
-
     std::vector<SXMLEntity> Data;
     size_t CurrentIndex;
 
-    // Fix: Call CXMLReader's constructor explicitly
     MockXMLReader() : CXMLReader(nullptr), CurrentIndex(0) {}
 
     bool ReadEntity(SXMLEntity &entity, bool skipcdata = false) override {
@@ -31,23 +23,15 @@ public:
 
 class OpenStreetMapTest : public ::testing::Test {
 protected:
-    void SetUp() override {
-        // Setup code for OpenStreetMapTest
-    }
-
-    void TearDown() override {
-        // Cleanup code for OpenStreetMapTest
-    }
+    void SetUp() override {}
+    void TearDown() override {}
 };
 
 TEST_F(OpenStreetMapTest, TestNodeCount) {
     auto xmlReader = std::make_shared<MockXMLReader>();
-    
-    xmlReader->Data = {
-        { MockXMLReader::SXMLEntity::EType::StartElement, "node", {{"id", "1"}} },
-        { MockXMLReader::SXMLEntity::EType::StartElement, "node", {{"id", "2"}} },
-        { MockXMLReader::SXMLEntity::EType::StartElement, "node", {{"id", "3"}} }
-    };
+    xmlReader->Data = { {SXMLEntity::EType::StartElement, "node"},
+                         {SXMLEntity::EType::StartElement, "node"},
+                         {SXMLEntity::EType::StartElement, "node"} };
 
     COpenStreetMap osmMap(xmlReader);
     EXPECT_EQ(osmMap.NodeCount(), 3);
@@ -55,11 +39,8 @@ TEST_F(OpenStreetMapTest, TestNodeCount) {
 
 TEST_F(OpenStreetMapTest, TestWayCount) {
     auto xmlReader = std::make_shared<MockXMLReader>();
-
-    xmlReader->Data = {
-        { MockXMLReader::SXMLEntity::EType::StartElement, "way", {{"id", "10"}} },
-        { MockXMLReader::SXMLEntity::EType::StartElement, "way", {{"id", "11"}} }
-    };
+    xmlReader->Data = { {SXMLEntity::EType::StartElement, "way"},
+                         {SXMLEntity::EType::StartElement, "way"} };
 
     COpenStreetMap osmMap(xmlReader);
     EXPECT_EQ(osmMap.WayCount(), 2);
